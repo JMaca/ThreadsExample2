8db1518aa3ccfc98830ebc880ad322db9bbb34fd
JMaca <70349528+JMaca@users.noreply.github.com>
2024-11-18 11:03:15 -0500

    Todo 4 Completed . Could not find solution to adjust text size on NumberPicker wheel ,documentation unclear.

--- app/src/main/java/edu/farmingdale/threadsexample/countdowntimer/TimerScreen.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/countdowntimer/TimerScreen.kt
@@ -1,10 +1,7 @@
 package edu.farmingdale.threadsexample.countdowntimer
 
-import android.util.Log
+import android.util.Size
 import android.widget.NumberPicker
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,19 +10,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -52,7 +47,7 @@ fun TimerScreen(
             }
             Text(
                 text = timerText(timerViewModel.remainingMillis),
-                fontSize = 40.sp,
+                fontSize = 50.sp,
             )
         }
         TimePicker(
@@ -66,7 +61,7 @@ fun TimerScreen(
                 onClick = timerViewModel::cancelTimer,
                 modifier = modifier.padding(50.dp)
             ) {
-                Text("Cancel")
+                Text("Cancel", fontSize = 30.sp)
             }
         } else {
             Button(
@@ -76,19 +71,19 @@ fun TimerScreen(
                 onClick = timerViewModel::startTimer,
                 modifier = modifier.padding(top = 50.dp)
             ) {
-                Text("Start")
+                Text("Start", fontSize = 20.sp)
             }
         }
     }
 }
 
 
-
 fun timerText(timeInMillis: Long): String {
     val duration: Duration = timeInMillis.milliseconds
     return String.format(
-        Locale.getDefault(),"%02d:%02d:%02d",
-        duration.inWholeHours, duration.inWholeMinutes % 60, duration.inWholeSeconds % 60)
+        Locale.getDefault(), "%02d:%02d:%02d",
+        duration.inWholeHours, duration.inWholeMinutes % 60, duration.inWholeSeconds % 60
+    )
 }
 
 @Composable
@@ -108,7 +103,7 @@ fun TimePicker(
         modifier = Modifier.fillMaxWidth()
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("Hours")
+            Text("Hours", fontSize = 25.sp)
             NumberPickerWrapper(
                 initVal = hourVal,
                 maxVal = 99,
@@ -122,23 +117,23 @@ fun TimePicker(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(start = 16.dp, end = 16.dp)
         ) {
-            Text("Minutes")
+            Text("Minutes", fontSize = 25.sp)
             NumberPickerWrapper(
                 initVal = minVal,
                 onNumPick = {
                     minVal = it
                     onTimePick(hourVal, minVal, secVal)
-                }
+                },
             )
         }
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("Seconds")
+            Text("Seconds", fontSize = 25.sp)
             NumberPickerWrapper(
                 initVal = secVal,
                 onNumPick = {
                     secVal = it
                     onTimePick(hourVal, minVal, secVal)
-                }
+                },
             )
         }
     }
@@ -149,12 +144,12 @@ fun NumberPickerWrapper(
     initVal: Int = 0,
     minVal: Int = 0,
     maxVal: Int = 59,
-    onNumPick: (Int) -> Unit = {}
-) {
+    onNumPick: (Int) -> Unit = {},
+
+    ) {
     val numFormat = NumberPicker.Formatter { i: Int ->
         DecimalFormat("00").format(i)
     }
-
     AndroidView(
         factory = { context ->
             NumberPicker(context).apply {
@@ -166,4 +161,11 @@ fun NumberPickerWrapper(
             }
         }
     )
+}
+
+@Preview
+@Composable
+fun SeePreview() {
+    TimerScreen()
+
 }
