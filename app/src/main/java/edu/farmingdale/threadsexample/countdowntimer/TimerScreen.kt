9aebff32cf598bac46b0cd2fe77ee81ff0485685
JMaca <70349528+JMaca@users.noreply.github.com>
2024-11-18 21:50:03 -0500

    Todo6,7,8 partially completed. added a pause button but needs logic, added a if statement to play sound, requires sound mp3. Changed font color to red for remaining 10 seconds.

--- app/src/main/java/edu/farmingdale/threadsexample/countdowntimer/TimerScreen.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/countdowntimer/TimerScreen.kt
@@ -1,7 +1,6 @@
 package edu.farmingdale.threadsexample.countdowntimer
 
 import android.widget.NumberPicker
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,10 +11,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,7 +20,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,7 +31,6 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import java.text.DecimalFormat
 import java.util.Locale
 import kotlin.time.Duration
